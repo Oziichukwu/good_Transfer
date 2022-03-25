58d49677316ed85a3107c983f2856f4cf5a1dd22
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-25 06:51:09 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+import Nav from './components/shared/Nav';
+import FooterComponent from './components/shared/footerComponent'
+import Welcome from './components/Welcome';
+import Register from './components/Register';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateWallet from './components/dashboard/dashboardoperations/CreateWallet';
+import NotFound from './components/shared/NotFound';
+import { Provider } from 'react-redux';
+import store from './Store';
+import UpdateWallet from './components/dashboard/dashboardoperations/UpdateWallet';
+import Transaction from './components/transactions/Transaction';
+//import AddTransaction from './components/transactions/transactionoperations/AddTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route path="/" component={Nav} />
+        <Switch>
+          <Route path="/" exact component={Welcome} />
+          <Route path="/sign-up" exact component={Register} />
+          <Route path="/login" exact component={Login} /> */}
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/createwallet" exact component={CreateWallet} />
+          <Route path="/updatewallet/:id" exact component={UpdateWallet} />
+          <Route path="/transactions/:id" exact component={Transaction} />
+          {/* <Route path="/trns/add/:id" exact component={AddTransaction} /> */}
+          <Route path="/" component={NotFound} />
+        </Switch>
+        <Route path="/" component={FooterComponent}/>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
